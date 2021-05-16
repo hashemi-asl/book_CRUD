8c9077c368428f55a47804355edd9fce54f39bbe
Maryam Hashemi Asl <55248325+hashemi-asl@users.noreply.github.com>
2021-05-16 11:11:58 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BOOK</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+</head>
+<body style=" background: #159957; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #159957, #155799); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #159957, #155799); ;
+">
+<section class="container-fluid">
+    <div class="show">
+        <div class="row m-0 mt-5 p-6">
+            <div class="col-8 offset-2">
+                <h1 class="text-center mt-5 text-white">Hello.welcome to our page ;)</h1>
+                <h3 class="text-center mt-5 text-white"><a href="../user_registration/registration.php" class="text-white">register </a></h3>
+                <h3 class="text-center mt-5 text-white"><a href="../user_registration/login.php" class="text-white">login </a></h3>
+<!---->
+<!--                <h3 class="text-center mt-5 text-white"><a href="../books/add_books.php" class="text-white">add books</a></h3>-->
+<!--                <h3 class="text-center mt-5 text-white"><a href="../books/book_list.php" class="text-white">book list </a></h3>-->
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="../js/jquery-3.5.1.min.js"></script>
+<script src="../js/popper.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
